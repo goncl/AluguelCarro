cf0a02044ad57a1014a1e35d4d132f9e94fbe6a7
Celso Gonçalves <goncl7@gmail.com>
2020-08-01 22:25:02 -0300

    Criação dos repositórios de niveis de scesso

---
+++ AluguelCarro/AcessoDados/Interfaces/IRepositorioGenerico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AluguelCarro.AcessoDados.Interfaces
+{
+    public interface IRepositorioGenerico<IEntity> where IEntity : class
+    {
+        IQueryable<IEntity> PegarTodos();
+        Task<IEntity> PegarPeloId(int id);
+        Task<IEntity> PegarPeloId(string id);
+        Task Inserir(IEntity entity);
+        Task Atualizar(IEntity entity);
+        Task Excluir(int id);
+        Task Excluir(string id);
+    }
+}
---
+++ AluguelCarro/AcessoDados/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,118 @@
+﻿using AluguelCarro.AcessoDados.Interfaces;
+using AluguelCarro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AluguelCarro.AcessoDados.Repositorios
+{
+    public class RepositorioGenerico<IEntity> : IRepositorioGenerico<IEntity> where IEntity : class
+    {
+        private readonly Contexto _contexto;
+
+        public RepositorioGenerico (Contexto contexto) 
+        {
+            _contexto = contexto;
+        }
+
+        public async Task Atualizar(IEntity entity)
+        {
+            try
+            {
+                _contexto.Set<IEntity>().Update(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(int id)
+        {
+            try
+            {
+                var entity = await PegarPeloId(id);
+                _contexto.Set<IEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(string id)
+        {
+            try
+            {
+                var entity = await PegarPeloId(id);
+                _contexto.Set<IEntity>().Remove(entity);
+                await _contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Inserir(IEntity entity)
+        {
+            try
+            {
+                await _contexto.Set<IEntity>().AddAsync(entity);
+                await _contexto.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<IEntity> PegarPeloId(int id)
+        {
+            try
+            {
+                return await _contexto.Set<IEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<IEntity> PegarPeloId(string id)
+        {
+            try
+            {
+                return await _contexto.Set<IEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IQueryable<IEntity> PegarTodos()
+        {
+            try
+            {
+                return _contexto.Set<IEntity>();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
